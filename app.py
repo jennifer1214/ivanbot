25d4d05b677a90d96ca4892f98fb4fe2ab771e9a
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-21 16:26:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
--- coding: utf-8 --
- """
- Created on Wed Jun  2 21:16:35 2021
- @author: Ivan
- 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
- Line Bot聊天機器人
- 第四章 選單功能
- 選擇按鈕ConfirmTemplate
- """
- 
-#  載入LineBot所需要的套件
- from flask import Flask, request, abort
- from linebot import (
-     LineBotApi, WebhookHandler
- )
- from linebot.exceptions import (
-     InvalidSignatureError
- )
- from linebot.models import *
- import re
- app = Flask(name)
- 
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('2bA2+2BpXpPhMxU5Mn6MJNanrwhM75WyW/bFDHUjbYIrdB8cufjwH2MocJllX7W/0wnv55EIZtJUVCn5M2/kG8N4tqPx2coDmGFfFdBZPJp64AfGRrkFpn3T5Bs9C06KlgwPTZrRFHAzdG3Xz90ReQdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
@@ -27,40 +18,33 @@ handler = WebhookHandler('7ab781240bed864ae1ae0e554acf3475')
 
 line_bot_api.push_message('Ufa79e88066b7a65bae8d131a1f1f9a0c', TextSendMessage(text='你可以開始了'))
 
-#  監聽所有來自 /callback 的 Post Request
- @app.route("/callback", methods=['POST'])
- def callback():
-     # get X-Line-Signature header value
-     signature = request.headers['X-Line-Signature']
- # get request body as text body = request.get_data(as_text=True) app.logger.info("Request body: " + body) # handle webhook body try:     handler.handle(body, signature) except InvalidSignatureError:     abort(400) return 'OK'
- # 訊息傳遞區塊
- # 基本上程式編輯都在這個function
- @handler.add(MessageEvent, message=TextMessage)
- def handle_message(event):
-     message = text=event.message.text
-     if re.match('告訴我秘密',message):
-         confirm_template_message = TemplateSendMessage(
-             alt_text='問問題',
-             template=ConfirmTemplate(
-                 text='你喜這堂課嗎？',
-                 actions=[
-                     PostbackAction(
-                         label='喜歡',
-                         display_text='超喜歡',
-                         data='action=其實不喜歡'
-                     ),
-                     MessageAction(
-                         label='愛',
-                         text='愛愛'
-                     )
-                 ]
-             )
-         )
-         line_bot_api.reply_message(event.reply_token, confirm_template_message)
-     else:
-         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
- # 主程式
- import os
- if name == "main":
-     port = int(os.environ.get('PORT', 5000))
-     app.run(host='0.0.0.0', port=port)
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
