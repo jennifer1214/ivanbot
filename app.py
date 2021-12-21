73a57b5f47148970d3901154596a4137cf4d3922
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-21 16:55:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ line_bot_api.push_message('Ufa79e88066b7a65bae8d131a1f1f9a0c', TextSendMessage(t
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
- def callback():
+def callback():
      # get X-Line-Signature header value
      signature = request.headers['X-Line-Signature']
  # get request body as text body = request.get_data(as_text=True) app.logger.info("Request body: " + body) # handle webhook body try:     handler.handle(body, signature) except InvalidSignatureError:     abort(400) return 'OK'
  # 訊息傳遞區塊
  # 基本上程式編輯都在這個function
- @handler.add(MessageEvent, message=TextMessage)
- def handle_message(event):
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
      message = TextSendMessage(text=event.message.text)
      if re.match('告訴我秘密',message):
          confirm_template_message = TemplateSendMessage(
@@ -51,7 +51,7 @@ line_bot_api.push_message('Ufa79e88066b7a65bae8d131a1f1f9a0c', TextSendMessage(t
      else:
          line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
  # 主程式
- import os
- if __name__ == "__main__":
+import os
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
