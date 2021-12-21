f55a3a72c66efdb5c5ff177c2e2dbfa56c049f6b
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-21 22:34:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -22,32 +22,32 @@ line_bot_api.push_message('Ufa79e88066b7a65bae8d131a1f1f9a0c', TextSendMessage(t
 @app.route("/callback", methods=['POST'])
 def callback():
      # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+     signature = request.headers['X-Line-Signature']
  # get request body as text body = request.get_data(as_text=True) app.logger.info("Request body: " + body) # handle webhook body try:     handler.handle(body, signature) except InvalidSignatureError:     abort(400) return 'OK'
  # 訊息傳遞區塊
  # 基本上程式編輯都在這個function
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message =text=event.message.text
-    if re.match('告訴我秘密',message):
-        confirm_template_message = TemplateSendMessage(
-            alt_text='問問題',
-            template=ConfirmTemplate(
-                text='你喜這堂課嗎？',
-                actions=[
-                    PostbackAction(
-                        label='喜歡',
-                        display_text='超喜歡',
-                        data='action=其實不喜歡'
-                    ),
-                    MessageAction(
-                        label='愛',
-                        text='愛愛'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, confirm_template_message)
+     message =text=event.message.text
+     if re.match('告訴我秘密',message):
+         confirm_template_message = TemplateSendMessage(
+             alt_text='問問題',
+             template=ConfirmTemplate(
+                 text='你喜這堂課嗎？',
+                 actions=[
+                     PostbackAction(
+                         label='喜歡',
+                         display_text='超喜歡',
+                         data='action=其實不喜歡'
+                     ),
+                     MessageAction(
+                         label='愛',
+                         text='愛愛'
+                     )
+                 ]
+             )
+         )
+         line_bot_api.reply_message(event.reply_token, confirm_template_message)
      else:
          line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
  # 主程式
