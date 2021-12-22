4c233c007027daad4606930a4215177c30545882
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-22 16:16:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -52,34 +52,41 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-    if re.match('告訴我秘密',message):
+    if re.match('[^開始]',message):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    else:
         buttons_template_message = TemplateSendMessage(
-        alt_text='這個看不到',
+        alt_text='問問題',
         template=ButtonsTemplate(
-            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            #thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
             title='行銷搬進大程式',
             text='選單功能－TemplateSendMessage',
             actions=[
                 PostbackAction(
-                    label='偷偷傳資料',
-                    display_text='檯面上',
-                    data='action=檯面下'
+                    label='A',
+                    display_text='A',
+                    data='action=1'
                 ),
-                MessageAction(
-                    label='光明正大傳資料',
-                    text='我就是資料'
+                PostbackAction(
+                    label='B',
+                    display_text='B',
+                    data='action=2'
                 ),
-                URIAction(
-                    label='行銷搬進大程式',
-                    uri='https://marketingliveincode.com/'
+                PostbackAction(
+                    label='C',
+                    display_text='C',
+                    data='action=3'
+                )
+                ,PostbackAction(
+                    label='D',
+                    display_text='D',
+                    data='action=4'
                 )
             ]
         )
     )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-
+        
 #主程式
 import os
 if __name__ == "__main__":
