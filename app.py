2cc52fbe11e81accc5574231ee04eba99eb9c573
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-22 23:55:14 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-    if re.match('[^開始]',message):
+    if message!='開始': #re.match('[^開始]',message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
     else:
         buttons_template_message = TemplateSendMessage(
