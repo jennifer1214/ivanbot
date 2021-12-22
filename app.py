cb6bb74b1db552dd55d41d66332cec526cdf0785
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-22 15:25:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -50,43 +50,104 @@ def callback():
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = text=event.message.text
-    if re.match('[^開始]',message):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-    else:
-        buttons_template_message = TemplateSendMessage(
-            alt_text='問問題',
-            template=ButtonsTemplate(
-                title='敘述',
-                text='請選擇',
-                actions=[
-                    PostbackAction(
-                        label='A',
-                        display_text='A',
-                        data='action=1'
-                    ),
-                    PostbackAction(
-                        label='B',
-                        display_text='B',
-                        data='action=2'
-                    ),
-                    PostbackAction(
-                        label='C',
-                        display_text='C',
-                        data='action=3'
-                    ),
-                    PostbackAction(
-                        label='D',
-                        display_text='D',
-                        data='action=4'
+def callback(request):
+ 
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+ 
+        try:
+            events = parser.parse(body, signature)  # 傳入的事件
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+ 
+        for event in events:
+            if isinstance(event, MessageEvent):  # 如果有訊息事件
+ 
+                if event.message.text == "哈囉":
+ 
+                    line_bot_api.reply_message(  # 回復傳入的訊息文字
+                        event.reply_token,
+                        TemplateSendMessage(
+                            alt_text='Buttons template',
+                            template=ButtonsTemplate(
+                                title='Menu',
+                                text='請選擇地區',
+                                actions=[
+                                    PostbackTemplateAction(
+                                        label='台北市',
+                                        text='台北市',
+                                        data='A&台北市'
+                                    ),
+                                    PostbackTemplateAction(
+                                        label='台中市',
+                                        text='台中市',
+                                        data='A&台中市'
+                                    ),
+                                    PostbackTemplateAction(
+                                        label='高雄市',
+                                        text='高雄市',
+                                        data='A&高雄市'
+                                    )
+                                ]
+                            )
+                        )
                     )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
-
-
+            elif isinstance(event, PostbackEvent):  # 如果有回傳值事件
+ 
+                if event.postback.data[0:1] == "A":  # 如果回傳值為「選擇地區」
+ 
+                    area = event.postback.data[2:]  # 透過切割字串取得地區文字
+ 
+                    line_bot_api.reply_message(   # 回復「選擇美食類別」按鈕樣板訊息
+                        event.reply_token,
+                        TemplateSendMessage(
+                            alt_text='Buttons template',
+                            template=ButtonsTemplate(
+                                title='Menu',
+                                text='請選擇美食類別',
+                                actions=[
+                                    PostbackTemplateAction(  # 將第一步驟選擇的地區，包含在第二步驟的資料中
+                                        label='火鍋',
+                                        text='火鍋',
+                                        data='B&' + area + '&火鍋'
+                                    ),
+                                    PostbackTemplateAction(
+                                        label='早午餐',
+                                        text='早午餐',
+                                        data='B&' + area + '&早午餐'
+                                    ),
+                                    PostbackTemplateAction(
+                                        label='約會餐廳',
+                                        text='約會餐廳',
+                                        data='B&' + area + '&約會餐廳'
+                                    )
+                                ]
+                            )
+                        )
+                    )
+ 
+                elif event.postback.data[0:1] == "B":  # 如果回傳值為「選擇美食類別」
+ 
+                    result = event.postback.data[2:].split('&')  # 回傳值的字串切割
+ 
+                    food = IFoodie(
+                        result[0],  # 地區
+                        result[1]  # 美食類別
+                    )
+ 
+                    line_bot_api.reply_message(  # 回復訊息文字
+                        event.reply_token,
+                        # 爬取該地區正在營業，且符合所選擇的美食類別的前五大最高人氣餐廳
+                        TextSendMessage(text=food.scrape())
+                    )
+ 
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
+        
 #主程式
 import os
 if __name__ == "__main__":
