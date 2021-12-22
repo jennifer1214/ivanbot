a47fe8baf4a6723d89fce47f32d4af1950f25812
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-22 11:56:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def handle_message(event):
             )
         )
         line_bot_api.reply_message(event.reply_token, confirm_template_message)
+        message = text=event.message.text
         if re.match('[我選擇男生]',message):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
         else:
