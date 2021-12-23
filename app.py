54ee5b2d59e28b37935b1f65a17e71288cbdd118
jennifer1214 <96033287+jennifer1214@users.noreply.github.com>
2021-12-23 12:01:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 21:16:35 2021
+
+@author: Ivan
+版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+
+Line Bot聊天機器人
+第四章 選單功能
+選擇按鈕ConfirmTemplate
+"""
+#載入LineBot所需要的套件
 import os
+import psycopg2
+
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-import configparser
-
-from custom_models import utils, PhoebeTalks
-
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import re
 app = Flask(__name__)
 
-# LINE 聊天機器人的基本資料
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('2bA2+2BpXpPhMxU5Mn6MJNanrwhM75WyW/bFDHUjbYIrdB8cufjwH2MocJllX7W/0wnv55EIZtJUVCn5M2/kG8N4tqPx2coDmGFfFdBZPJp64AfGRrkFpn3T5Bs9C06KlgwPTZrRFHAzdG3Xz90ReQdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
@@ -19,15 +32,17 @@ handler = WebhookHandler('7ab781240bed864ae1ae0e554acf3475')
 
 line_bot_api.push_message('Ufa79e88066b7a65bae8d131a1f1f9a0c', TextSendMessage(text='開始你的表演，請輸入：開始'))
 
-
-# 接收 LINE 的資訊
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -35,26 +50,38 @@ def callback():
 
     return 'OK'
 
-# 紀錄資料
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
-def reply_text_message(event):
-    
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+def handle_message(event):
+    message = text=event.message.text
+    if message!='開始': #re.match('[^開始]',message):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    else:
+        buttons_template_message = TemplateSendMessage(
+            alt_text='問問題',
+            template=ButtonsTemplate(
+                #thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+                title='行銷搬進大程式',
+                text='選單功能－TemplateSendMessage',
+                actions=[
+                    PostbackAction(
+                        label='A',
+                        display_text='A',
+                        data='action=1'
+                    ),
+                    PostbackAction(
+                        label='B',
+                        display_text='B',
+                        data='action=2'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
         
-        reply = False
-        
-        if not reply:
-            reply = PhoebeTalks.insert_record(event)
-        
-        if not reply:
-            reply = PhoebeTalks.img_search(event)
-                    
-        if not reply:
-            reply = PhoebeTalks.img_search(event)
-                    
-        if not reply:
-            reply = PhoebeTalks.pretty_echo(event)
-            
-
+#主程式
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
